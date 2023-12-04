f1543100535c8eb397b3dc199b028dd4ea65bebe
saladk21 <72814229+saladk21@users.noreply.github.com>
2023-12-04 11:50:17 +0300

    test

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Hospital_Management_System_V0.1"
+include(":app")
+ 
