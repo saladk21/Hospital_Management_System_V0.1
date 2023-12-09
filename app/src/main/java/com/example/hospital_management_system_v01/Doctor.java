ab6f4dd9753a6478ddb0b9f00b0a8d8820b6634d
blank <eamo00@hotmail.com>
2023-12-09 18:24:14 +0300

    the list works but with wrong parameters

--- app/src/main/java/com/example/hospital_management_system_v01/Doctor.java
+++ app/src/main/java/com/example/hospital_management_system_v01/Doctor.java
@@ -1,32 +1,49 @@
 package com.example.hospital_management_system_v01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.database.Cursor;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class Doctor extends AppCompatActivity {
 
+
+    RecyclerView recyclerView;
+    FloatingActionButton add_button;
     Database myDB;
-    ArrayList<String> book_id, book_title, book_author, book_pages;
+    ArrayList<String> columnID, pName, pID, docID;
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor);
 
+        Log.d("DoctorActivity", "Doctor activity created");
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+
         //initialize database
         myDB = new Database(Doctor.this);
-        book_id = new ArrayList<>();
-        book_title = new ArrayList<>();
-        book_author = new ArrayList<>();
-        book_id = new ArrayList<>();
+        columnID = new ArrayList<>();
+        pName = new ArrayList<>();
+        pID = new ArrayList<>();
+        docID = new ArrayList<>();
 
+        StoreDataInArrays();
+
+        customAdapter = new CustomAdapter(Doctor.this, columnID, pName, pID, docID);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(Doctor.this));
     }
 
     void StoreDataInArrays(){
@@ -36,11 +53,16 @@ public class Doctor extends AppCompatActivity {
         }
         else {
             while(cursor.moveToNext()){
-                book_id.add(cursor.getString(0));
-                book_title.add(cursor.getString(1));
-                book_author.add(cursor.getString(2));
-                book_pages.add(cursor.getString(3));
+                columnID.add(cursor.getString(0));
+                pName.add(cursor.getString(1));
+                pID.add(cursor.getString(2));
+                docID.add(cursor.getString(3));
             }
+            Log.d("DoctorActivity", "columnID size: " + columnID.size());
+            Log.d("DoctorActivity", "pName size: " + pName.size());
+            Log.d("DoctorActivity", "pID size: " + pID.size());
+            Log.d("DoctorActivity", "docID size: " + docID.size());
         }
+
     }
 }
