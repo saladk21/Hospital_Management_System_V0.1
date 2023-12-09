e2e4dbf7c50b08a8c23392edb55ceef8af6fbbe3
blank <eamo00@hotmail.com>
2023-12-09 20:46:56 +0300

    merged with working button

--- app/src/main/java/com/example/hospital_management_system_v01/Database.java
+++ app/src/main/java/com/example/hospital_management_system_v01/Database.java
@@ -1,5 +1,6 @@
 package com.example.hospital_management_system_v01;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
